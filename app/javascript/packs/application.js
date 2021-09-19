2217c4f1820a21e259c502e3aa97d3f04643f2af
Gise Ares <gise.ares@gmail.com>
2021-09-19 17:52:16 -0300

    muteo typejs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,13 +26,13 @@ import "bootstrap";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initUpdateNavbarOnScroll } from '../components/navbar.js';
-import { loadDynamicBannerText } from '../components/banner';
+//import { loadDynamicBannerText } from '../components/banner';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your JS functions here
   initUpdateNavbarOnScroll();
   loadDynamicAboutUsText();
-  loadDynamicBannerText();
+  //loadDynamicBannerText();
 
 
 });

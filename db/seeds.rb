1f9d1be42057fa0ca5b4c5a82ff2a0d4e5634008
Gise Ares <gise.ares@gmail.com>
2021-09-21 19:27:33 -0300

    Second gallery up

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,182 +5,217 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
 
 puts "Limpiando la DB..."
 Portfolio.destroy_all
 
-puts "Creando Productos..."
+puts "Creando Servicios..."
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_design_daria-nepriakhina_01_mlgtig')
 design = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Design Thinking",
   content: "Foco en los fundamentos teóricos y prácticos de todas las etapas del Design Thinking, enfocados en la creación de propuestas basadas en las experiencias y la mirada de usuarios.",
-  url_image: 'prd_design_daria-nepriakhina.jpg'
+  url_image: 'daria-nepriakhina'
 )
+design.photo.attach(io: file, filename: 'prd_design_daria-nepriakhina_01_mlgtig', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_business_canva_kelly-sikkema_02_h9nymc')
 canva = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Business Model Canva",
   content: "Permite tener una mayor claridad en cómo estudiar al público objetivo, definir qué le
   ofrece su producto o servicio al mercado, cómo venderlo y obtener ingresos.",
-  url_image: 'prd_business_canva_kelly-sikkema.jpg'
+  url_image: 'kelly-sikkema'
 )
+canva.photo.attach(io: file, filename: 'prd_business_canva_kelly-sikkema_02_h9nymc', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_lean_jake-hills_03_unbpil')
 lean = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Lean Startup",
   content: "Conocer la metodología Lean Startup. Emprender pensando en el cliente a través del
   Customer Development, así como definir y crear un Producto Mínimo Variable (PMV).",
-  url_image: 'prd_lean_jake-hills.jpg'
+  url_image: 'jake-hills'
 )
+lean.photo.attach(io: file, filename: '
+prd_lean_jake-hills_03_unbpil', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_project_austin-distel_04_oqnghf')
 project = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Project Management",
   content: "Consolidar los conocimientos de gestión de proyectos de los asistentes y aplicar las
   metodologías de gestión de proyectos a un proyecto concreto.",
-  url_image: 'prd_project_austin-distel.jpg'
+  url_image: 'austin-distel'
 )
+project.photo.attach(io: file, filename: 'prd_project_austin-distel_04_oqnghf', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_cambio_kalei-peek_05_akrk3x')
 cambio = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Gestión de Cambio Organizacional",
   content: "Impulsar un proceso de cambio no siempre es fácil, requiere de liderazgo positivo y de
   personas capacitadas para tener un cambio alineado a las competencias que busca el
   mercado..",
-  url_image: 'prd_cambio_kalei-peek.jpg'
+  url_image: 'kalei-peek'
 )
+cambio.photo.attach(io: file, filename: 'prd_cambio_kalei-peek_05_akrk3x', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_liderazgo_jehyun-sung_06_sq7hjf')
 liderazgo = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Taller de Liderazgo",
   content: "Conocer las fortalezas del grupo de trabajadores para obtener lo mejor de cada uno y
   destinar estas capacidades hacia los fines y objetivos empresariales.",
-  url_image: 'prd_liderazgo_jehyun-sung.jpg'
+  url_image: 'jehyun-sung'
 )
+liderazgo.photo.attach(io: file, filename: 'prd_liderazgo_jehyun-sung_06_sq7hjf', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_experience_blake-wisz_07_pwtdpn')
 customer = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Customer Experience",
   content: "En este taller, utilizaremos un marco, adaptado de Customer Journey Mapping, para
   enfocar y organizar el pensamiento de los estudiantes sobre las implicaciones operativas
   de sus proyectos de marca.",
-  url_image: 'prd_experience_blake-wisz.jpg'
+  url_image: 'blake-wisz'
 )
+customer.photo.attach(io: file, filename: 'prd_experience_blake-wisz_07_pwtdpn', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_negociacion_priscilla-du-preez_08_ogfril')
 negociacion = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Negociación",
   content: "El taller ofrece a los participantes los conocimientos y las herramientas necesarios para ejercitar y desarrollar sus habilidades de negociación.",
-  url_image: 'prd_negociacion_priscilla-du-preez.jpg'
+  url_image: 'priscilla-du-preez'
 )
+negociacion.photo.attach(io: file, filename: 'prd_negociacion_priscilla-du-preez_08_ogfril', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_conversaciones_christina-wocintechchat_09_knkjr7')
 conversaciones = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Conversaciones Difíciles",
   content: "Como siempre que hablamos, lo hacemos desde algún estado emocional, de la capacidad   de gestionar nuestras propias emociones depende, en gran medida, el éxito de nuestras conversaciones.",
-  url_image: 'prd_conversaciones_christina-wocintechchat.jpg'
+  url_image: 'christina-wocintechchat'
 )
+conversaciones.photo.attach(io: file, filename: 'prd_conversaciones_christina-wocintechchat_09_knkjr7', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_escucha_mimi-thian_10_kan9bf')
 escucha = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Escucha Activa",
   content: "La calidad en el servicio entregado a los clientes es una preocupación constante de las
   empresas para mantenerlos satisfechos y generar más clientes por el servicio entregado.
   Puede incluir calibración de descargos",
-  url_image: 'prd_escucha_mimi-thian.jpg'
+  url_image: 'mimi-thian'
 )
+escucha.photo.attach(io: file, filename: 'prd_escucha_mimi-thian_10_kan9bf', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_objecciones_afif-kusuma_11_l1vqmm')
 objecciones = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Manejo de Objeciones",
   content: "Entrenar las diferentes fases de la negociación y practicar las principales objeciones y como
   responderlas.",
-  url_image: 'prd_objecciones_afif-kusuma.jpg'
+  url_image: 'afif-kusuma'
 )
+objecciones.photo.attach(io: file, filename: 'prd_objecciones_afif-kusuma_11_l1vqmm', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_neuro_bret-kavanaugh_12_mn52fq')
 neuroventas = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Neuroventas",
   content: "Cómo potenciar nuestro rendimiento intelectual y poner en valor las inteligencias y
   capacidades que poseemos para aplicarlas en la neuroventa.",
-  url_image: 'prd_neuro_bret-kavanaugh.jpg'
+  url_image: 'bret-kavanaugh'
 )
+neuroventas.photo.attach(io: file, filename: 'prd_neuro_bret-kavanaugh_12_mn52fq', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_ventas_nam-hoang_13_cym6wl')
 ventas = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Taller de Ventas",
   content: "Tiene como objetivo formar a un equipo comercial para dominar los métodos, técnicas y
   habilidades que promuevan la venta y la fidelización del cliente.",
-  url_image: 'prd_ventas_nam-hoang.jpg'
+  url_image: 'nam-hoang_13'
 )
+ventas.photo.attach(io: file, filename: 'prd_ventas_nam-hoang_13_cym6wl', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_presentacones_matthew-osborn_14_pkt1hb')
 presentaciones = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Presentaciones Eficaces",
   content: "El Taller brinda las herramientas esenciales para convertir tus presentaciones en
   presentaciones de alto impacto.",
-  url_image: 'prd_presentacones_matthew-osborn.jpg'
+  url_image: 'matthew-osborn.jpg'
 )
+presentaciones.photo.attach(io: file, filename: 'prd_presentacones_matthew-osborn_14_pkt1hb', content_type: 'image/png')
 
-
-
-
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_cliente_arlington-research_15_vc8bau')
 cliente = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Atención al Cliente",
   content: "Desarrollar habilidades para tratar efectivamente a las personas desde la parte humana y
   poder identificar las necesidades más ocultas, desde la escucha empática, el respeto y el
   logro de los objetivos de venta..",
-  url_image: 'prd_cliente_arlington-research.jpg'
+  url_image: 'arlington-research'
 )
+cliente.photo.attach(io: file, filename: 'prd_cliente_arlington-research_15_vc8bau', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_rrss_adem-ay_16_ckhx8k')
 redes = Portfolio.create!(
   category: "Formacion_Profesional",
   name: "Redes Sociales",
   content: "Adquirir y aplicar nuevos conocimientos, habilidades y destrezas en el diseño y gestión
   estratégica de redes sociales.",
-  url_image: 'prd_rrss_adem-ay.jpg'
+  url_image: 'adem-ay'
 )
+redes.photo.attach(io: file, filename: 'prd_rrss_adem-ay_16_ckhx8k', content_type: 'image/png')
 
-
-
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_coaching_empresrio_alejandro-escamilla_17_bdvbww')
 empresarial = Portfolio.create!(
   category: "Coaching",
   name: "Coaching Empresario",
   content: "El coach te ayuda a ver su entorno, abre su visión y le ayuda a pensar en tus habilidades para
   desarrollar. Un valioso compañero externo que camina a su lado por un tiempo bien definido
   para que su personal encuentre nuevas soluciones.",
-  url_image: 'prd_coaching_empresrio_alejandro-escamilla.jpg'
+  url_image: 'alejandro-escamilla'
 )
+empresarial.photo.attach(io: file, filename: 'prd_coaching_empresrio_alejandro-escamilla_17_bdvbww', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_coaching_equipos_jamie-templeton_18_yo051f')
 equipos = Portfolio.create!(
   category: "Coaching",
   name: "Coaching Equipos",
   content: "El objetivo de coaching de equipos es guiar a un equipo a que defina su meta común, identificar
   que limita su desarrollo y poner en marcha una transformación que les permita maximizar su
   potencial y optimizar su rendimiento.",
-  url_image: 'prd_coaching_equipos_jamie-templeton.jpg'
+  url_image: 'jamie-templeton'
 )
+equipos.photo.attach(io: file, filename: 'prd_coaching_equipos_jamie-templeton_18_yo051f', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_procesos_clayton-robbins_19_gmpefx')
 procesos = Portfolio.create!(
   category: "Consultoría",
   name: "Procesos de Negocios",
   content: "A partir de las necesidades de la gerencia y una serie de entrevistas in situ se trabaja en identificar y abordar las ineficiencias que desperdician recursos/capital y añaden costos.",
-  url_image: 'prd_procesos_clayton-robbins.jpg'
+  url_image: 'clayton-robbins'
 )
+procesos.photo.attach(io: file, filename: 'prd_procesos_clayton-robbins_19_gmpefx', content_type: 'image/png')
 
+file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632258593/prd_diving_laya-clode_20_ozuylj')
 consultoria = Portfolio.create!(
   category: "Consultoría",
   name: "Consultoría Centros de Buceo",
   content: "Los programas pueden adaptarse según las necesidades de cada centro
   - Consultoría integral sobre su centro
   - Armado de plan de negocios / Business Case",
-  url_image: 'prd_diving_laya-clode.jpg'
+  url_image: 'laya-clode'
 )
+consultoria.photo.attach(io: file, filename: 'prd_diving_laya-clode_20_ozuylj', content_type: 'image/png')
 
-puts "Open Uri"
-require "open-uri"
-
+puts "Creando Gallery"
 file = URI.open('https://res.cloudinary.com/giseares/image/upload/v1632154876/proyecto_gallery_01L_zo8rui.jpg')
 uno = Portfolio.create!( 
   category: "Gallery",
